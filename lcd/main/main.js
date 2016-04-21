6a1a11a43805c86d8b38732dc2ebb53b713e9d7e
XLuoChen <1425145741@qq.com>
2016-04-21 16:24:41 +0800

    commit lcd for lcd-digits-XLuoChen

---
+++ lcd/main/main.js
@@ -0,0 +1,59 @@
+function printLcdDigits(characters) {
+    var digits = splitToDigits(characters);
+
+    var grids = loadAllDigitsGrids();
+    var lattices = buildLattices(digits,grids);
+
+    var lcdDigit = buildLcdDigit(lattices);
+    console.log(lcdDigit);
+}
+
+function splitToDigits(characters) {
+    var digits = [];
+    
+    digits = characters.split('');
+    
+    return digits;
+}
+function getGridForDigit(digit,grids) {
+    for(var i = 0;i < grids.length;i++){
+        if(grids[i].value === parseInt(digit)){
+
+            return grids[i];
+        }
+    }
+}
+function buildLattices(digits,grids) {
+    var lattices = [];
+    digits.forEach(function (digit) {
+        var girdForDigit = getGridForDigit(digit,grids);
+        lattices.push(girdForDigit);
+    });
+
+    return lattices;
+}
+
+function generateText(lattices) {
+    var text = '';
+
+    lattices.forEach(function (lattice) {
+        text += lattice.pattern.firstLine + ' ';
+    });
+    text += '\n';
+    lattices.forEach(function (lattice) {
+        text += lattice.pattern.secondLine + ' ';
+    });
+    text += '\n';
+    lattices.forEach(function (lattice) {
+        text += lattice.pattern.thirdLine + ' ';
+    });
+
+    return text;
+}
+function buildLcdDigit(lattices) {
+    var lcdDigit = '';
+
+    lcdDigit = generateText(lattices);
+
+    return lcdDigit;
+}
