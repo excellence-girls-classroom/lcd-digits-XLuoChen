851afd330fb3db498ad303d081762e22c0008448
XLuoChen <1425145741@qq.com>
2016-04-21 17:18:21 +0800

    modified main.js

--- lcd/main/main.js
+++ lcd/main/main.js
@@ -60,3 +60,18 @@ function buildLcdDigit(lattices) {
 
     return lcdDigit;
 }
+
+function loadAllDigitsGrids() {
+    return [
+        {value:0,pattern:{firstLine:'._.',secondLine:'|.|',thirdLine:'|_|'}},
+        {value:1,pattern:{firstLine:'...',secondLine:'..|',thirdLine:'..|'}},
+        {value:2,pattern:{firstLine:'._.',secondLine:'._|',thirdLine:'|_.'}},
+        {value:3,pattern:{firstLine:'._.',secondLine:'._|',thirdLine:'._|'}},
+        {value:4,pattern:{firstLine:'...',secondLine:'|_|',thirdLine:'..|'}},
+        {value:5,pattern:{firstLine:'._.',secondLine:'|_.',thirdLine:'._|'}},
+        {value:6,pattern:{firstLine:'._.',secondLine:'|_.',thirdLine:'|_|'}},
+        {value:7,pattern:{firstLine:'._.',secondLine:'..|',thirdLine:'..|'}},
+        {value:8,pattern:{firstLine:'._.',secondLine:'|_|',thirdLine:'|_|'}},
+        {value:9,pattern:{firstLine:'._.',secondLine:'|_|',thirdLine:'..|'}}
+    ];
+}
